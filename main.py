4945185e806f62b993cfaaacbd648d92dd1bbb6d
vonskeets <zach.k.marvin@gmail.com>
2025-07-01 15:31:00 -0700

    final update on bookbot

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 from stats import main
 
 
--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file = f.read()
@@ -7,8 +10,8 @@ def get_book_text(filepath):
 
 
 
-def dictcount():
-    bookText = (get_book_text("./books/frankenstein.txt"))
+def dictcount(filepath):
+    bookText = get_book_text(filepath)
     char_counts = {}
 
     words = bookText.lower()
@@ -22,18 +25,39 @@ def dictcount():
     return char_counts
 
 
-def main():
-    bookText = get_book_text("./books/frankenstein.txt")
+def dictsort(filepath):
+    sortBook = []
 
-    bookCharcount = dictcount()
+    bookCharcount = dictcount(filepath)
+
+    for char, count in bookCharcount.items():
+        sortBook.append({"char": char, "num": count})
+
+    sortBook.sort(reverse=True, key=lambda item: item["num"])
+
+    return sortBook
     
+
+def main():
+
+    filepath = sys.argv[1]
+
+    bookText = get_book_text(filepath)
+
+    bookCharsort = dictsort(filepath)
+  
+
     words = bookText.split()
 
     count = len(words)
 
-
-    print(f"{count} words found in the document.")
-    print(f"{bookCharcount}")
-
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}")
+    print("----------- Word Count ----------")
+    print(f"Found {count} total words")
+    print("--------- Character Count -------")
+    for item in bookCharsort:
+        if item["char"].isalpha():
+            print(f"{item['char']}: {item['num']}")
     
 main()
